ceb6fe03089ec6a58dffd30e5a6c1b1ff94b6d76
tatumks2 <tatuminabook@gmail.com>
2022-04-11 12:47:46 -0500

    game and piece classes

---
+++ chess.js
@@ -0,0 +1,121 @@
+const BOARD_SIZE = 8;
+
+class Piece {
+    constructor(game, x, y) {
+        this.game = game;
+        // pieces in the first two rows are white (0) and the last two are black (1)
+        this.color = Math.floor(x / 6);
+        this.x = x;
+        this.y = y;
+    }
+    
+    legal_square(x_1, y_1) {
+        // check square in on the board
+        if (x_1 < 0 || x_1 >= BOARD_SIZE || y_1 < 0 || y_1 >= BOARD_SIZE) {
+            return false;
+        }
+        // check square is does not have a piece of the same color
+        if (this.game.board[x_1][y_1] != null && this.game.board[x_1][y_1].color == this.color) {
+            return false;
+        }
+        // check move would move the piece
+        if (this.x == x_1 || this.y == y_1) {
+            return false;
+        }
+        return true;
+    }
+    
+    // knight move function
+    get_moves() {
+        let moves = Array();
+        for (let i = -1; i <= 1; i += 2) {
+            for (let j = -2; j <= 2; j += 4) {
+                // moves in x direction by one and y by two
+                let x_1 = this.x + i;
+                let y_1 = this.y + j;
+                if (this.legal_square(x_1, y_1)) {
+                    moves.push([this, x_1, y_1]);
+                }
+                // moves in y direction by two and y by one
+                x_1 = this.x + j;
+                y_1 = this.y + i;
+                if (this.legal_square(x_1, y_1)) {
+                    moves.push([this, x_1, y_1]);
+                }
+            }
+        } 
+        return moves;
+    }
+}
+
+class King extends Piece {
+    get_moves() {
+        let moves = Array();
+        for (let x_1 = this.x - 1; x_1 <= this.x + 1; x_1++) {
+            for (let y_1 = this.y - 1; y_1 <= this.y + 1; y_1++) {
+                // most of this should be in a function
+                if (this.legal_square(x_1, y_1)) {
+                    moves.push([this, x_1, y_1]);
+                }
+            }
+        } 
+        return moves;
+    }
+}
+
+// maybe board should be typed array 
+// const piece_row = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook];
+const piece_row = [Piece, Piece, Piece, Piece, King, Piece, Piece, Piece];
+let null_rows = [];
+for (i = 0; i < 6; i++) {
+    null_rows.push(Array(8).fill(null));
+}
+const STARTING_BOARD = [piece_row].concat(null_rows).concat([[...piece_row]]);
+
+class Game {
+    constructor(){
+        this.board = STARTING_BOARD;
+        for (let x = 0; x < 8; x++) {
+            for (let y = 0; y < 8; y++) {
+                if (this.board[x][y] != null) {
+                    this.board[x][y] = new this.board[x][y](this, x, y);
+                }
+            }
+        }
+        this.player = 0;
+    }
+    make_move(m) {
+        let [piece, x_1, y_1] = m;
+        let [x_0, y_0] = [piece.x, piece.y];
+        piece.x = x_1;
+        piece.y = y_1;
+        this.board[x_1][y_1] = piece;
+        this.board[x_0][y_0] = null;
+    }
+    make_random_move(color) {
+        let moves = [];
+        for (let x = 0; x < 8; x++){
+            for (let y = 0; y < 8; y++) {
+                if (this.board[x][y] != null) {
+                    let piece_moves = this.board[x][y].get_moves();
+                    if (moves.length == 0) {
+                        moves = piece_moves;
+                    } else {
+                        // something isn't working here
+                        moves.concat(piece_moves);
+                    }
+                    console.log(moves);
+                }
+            }
+        }
+        console.log('moves:', moves);
+        let m = moves[Math.floor(Math.random() * moves.length)];
+        console.log('m:', m);
+        this.make_move(m);
+    }
+}
+
+console.log(STARTING_BOARD);
+let game = new Game();
+game.make_random_move(0);
+console.log(game.board);
